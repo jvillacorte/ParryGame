805919c90bdaec82ccaef896c927349f0ab6dd90
jvillacorte <joseph.villacorte@gmail.com>
2025-09-22 01:16:42 -0700

    e

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 5f;
+    private Vector2 _movement;
+    private Rigidbody2D rb;
+    private Animator _animator;
+
+    private const string _horizontal = "Horizontal";
+    private const string _vertical = "Vertical";
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        _movement.Set(InputManager.Movement.x, InputManager.Movement.y);
+        rb.linearVelocity = _movement * _moveSpeed;
+
+        _animator.SetFloat(_horizontal, _movement.x);
+        _animator.SetFloat(_vertical, _movement.y);
+    }
+}
